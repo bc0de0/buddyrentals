becd746743ae8ed40d4e7658267ce26e18881b3e
bc0de0 <kagupte@gmail.com>
2025-07-28 11:58:39 +0530

    First Commit - Scaffolding and spring boot directory structure only

---
+++ src/main/java/com/godigit/buddyrentals/BuddyrentalsApplication.java
@@ -0,0 +1,13 @@
+package com.godigit.buddyrentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyrentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyrentalsApplication.class, args);
+	}
+
+}
